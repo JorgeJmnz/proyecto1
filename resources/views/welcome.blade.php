8e919b4300de519accac5a1a1ae92769b30a59eb
Emmanuelj1 <bremmgears@gmail.com>
2022-10-05 13:37:21 -0500

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Brayan Emmanuel López Cruz 
+                                    Brayan Emmanuel López Cruz
                                 </div>
                             </div>
                         </div>
