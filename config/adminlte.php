7bd585833ef4db9c17433a2436ba7b107f850fc6
Emmanuelj1 <bremmgears@gmail.com>
2022-10-06 11:55:40 -0500

    Boton CRUD

--- config/adminlte.php
+++ config/adminlte.php
@@ -330,6 +330,11 @@ return [
             'icon' => 'fas fa-fw fa-lock',
         ],
         [
+            'text' => 'add_elements',
+            'url'  => 'admin/settings',
+            'icon' => 'fas fa-fw fa-lock',
+        ],
+        [
             'text'    => 'multilevel',
             'icon'    => 'fas fa-fw fa-share',
             'submenu' => [
