6742799da539459faa3f67690267aef966d4eeab
JorgeJmnz <jorgejmnz08@gmail.com>
2022-10-12 10:53:39 -0500

    vistanueva

--- config/adminlte.php
+++ config/adminlte.php
@@ -253,7 +253,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => 'dashboard',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
@@ -313,7 +313,7 @@ return [
         ],
         [
             'text' => 'Dashboard',
-            'route' => 'home',
+            'url' => 'dashboard',
 
         ],
         ['header' => 'account_settings'],
