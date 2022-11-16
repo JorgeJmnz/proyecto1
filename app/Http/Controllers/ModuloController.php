0a584ed20e07885bf27f7c397fefbc690ca1f158
Emmanuelj1 <bremmgears@gmail.com>
2022-11-16 16:14:19 -0500

    1

--- app/Http/Controllers/ModuloController.php
+++ app/Http/Controllers/ModuloController.php
@@ -70,7 +70,7 @@ class ModuloController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified resource
      *
      * @param  int $id
      * @return \Illuminate\Http\Response
