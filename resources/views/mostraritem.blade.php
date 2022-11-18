ed24f538a980f71482a42ec8224f1b9f96826d78
Emmanuelj1 <bremmgears@gmail.com>
2022-11-17 23:44:02 -0500

    back

--- resources/views/mostraritem.blade.php
+++ resources/views/mostraritem.blade.php
@@ -2,7 +2,9 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
-
+<div class="float-right">
+                            <a class="btn btn-primary" href="{{ route('catalogo.index') }}"> Back</a>
+                        </div>
 <div>
   <div >
     <h5 class="card-title">{{ $modulo->nombre }}</h5>
