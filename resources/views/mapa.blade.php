54fd6906b32b016cf0abd801bdcfb281a5dac6ae
Emmanuelj1 <bremmgears@gmail.com>
2022-11-29 11:57:37 -0500

    COMENTARIOS NUEVOS

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -128,7 +128,7 @@
         background-image: url(marker.png);
       }
 
-      /* Marker tweaks */
+      
       .mapboxgl-popup {
         padding-bottom: 50px;
       }
@@ -181,9 +181,7 @@
     <script>
       mapboxgl.accessToken = 'pk.eyJ1Ijoiam9yZ2VqbW56MzAiLCJhIjoiY2w4MXBxdjdyMDJhNTN2cGh4emNmd3BkbCJ9.NFtlYZXXZqlJn5fVKzOuBQ';
 
-      /**
-       * Add the map to the page
-       */
+      
       const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
@@ -210,77 +208,54 @@
         ]
       };
 
-      /**
-       * Assign a unique id to each store. You'll use this `id`
-       * later to associate each point on the map with a listing
-       * in the sidebar.
-       */
+     
       stores.features.forEach((store, i) => {
         store.properties.id = i;
       });
 
-      /**
-       * Wait until the map loads to make changes to the map.
-       */
+     
       map.on('load', () => {
-        /**
-         * This is where your '.addLayer()' used to be, instead
-         * add only the source without styling a layer
-         */
+        
 
 
   map.addLayer({
     id: 'locations',
     type: 'circle',
-    /* Add a GeoJSON source containing place coordinates and information. */
+   
     source: {
       type: 'geojson',
       data: stores
     }
   });
 
-        /**
-         * Add all the things to the page:
-         * - The location listings on the side of the page
-         * - The markers onto the map
-         */
+        
         buildLocationList(stores);
         addMarkers();
       });
 
-      /**
-       * Add a marker to the map for every store listing.
-       **/
+      
       function addMarkers() {
-        /* For each feature in the GeoJSON object above: */
+      
         for (const marker of stores.features) {
-          /* Create a div element for the marker. */
+         
           const el = document.createElement('div');
-          /* Assign a unique `id` to the marker. */
+    
           el.id = `marker-${marker.properties.id}`;
-          /* Assign the `marker` class to each marker for styling. */
+          
           el.className = 'marker';
 
-          /**
-           * Create a marker using the div element
-           * defined above and add it to the map.
-           **/
+          
           new mapboxgl.Marker(el, { offset: [0, -23] })
             .setLngLat(marker.geometry.coordinates)
             .addTo(map);
 
-          /**
-           * Listen to the element and when it is clicked, do three things:
-           * 1. Fly to the point
-           * 2. Close all other popups and display popup for clicked store
-           * 3. Highlight listing in sidebar (and remove highlight for all other listings)
-           **/
+         
           el.addEventListener('click', (e) => {
-            /* Fly to the point */
+
             flyToStore(marker);
-            /* Close all other popups and display popup for clicked store */
+            
             createPopUp(marker);
-            /* Highlight listing in sidebar */
+         
             const activeItem = document.getElementsByClassName('active');
             e.stopPropagation();
             if (activeItem[0]) {
@@ -294,38 +269,30 @@
         }
       }
 
-      /**
-       * Add a listing for each store to the sidebar.
-       **/
+     
       function buildLocationList(stores) {
         for (const store of stores.features) {
-          /* Add a new listing section to the sidebar. */
+          
           const listings = document.getElementById('listings');
           const listing = listings.appendChild(document.createElement('div'));
-          /* Assign a unique `id` to the listing. */
+          
           listing.id = `listing-${store.properties.id}`;
-          /* Assign the `item` class to each listing for styling. */
+         
           listing.className = 'item';
 
-          /* Add the link to the individual listing created above. */
+          
           const link = listing.appendChild(document.createElement('a'));
           link.href = '#';
           link.className = 'title';
           link.id = `link-${store.properties.id}`;
           link.innerHTML = `${store.properties.Nombre}`;
 
-          /* Add details to the individual listing. */
+          
           const details = listing.appendChild(document.createElement('div'));
           details.innerHTML = `${store.properties.Descripcion}`;
          
 
-          /**
-           * Listen to the element and when it is clicked, do four things:
-           * 1. Update the `currentFeature` to the store associated with the clicked link
-           * 2. Fly to the point
-           * 3. Close all other popups and display popup for clicked store
-           * 4. Highlight listing in sidebar (and remove highlight for all other listings)
-           **/
+         
           link.addEventListener('click', function () {
             for (const feature of stores.features) {
               if (this.id === `link-${feature.properties.id}`) {
@@ -342,10 +309,7 @@
         }
       }
 
-      /**
-       * Use Mapbox GL JS's `flyTo` to move the camera smoothly
-       * a given center point.
-       **/
+     
       function flyToStore(currentFeature) {
         map.flyTo({
           center: currentFeature.geometry.coordinates,
@@ -353,9 +317,7 @@
         });
       }
 
-      /**
-       * Create a Mapbox GL JS `Popup`.
-       **/
+      
       function createPopUp(currentFeature) {
         const popUps = document.getElementsByClassName('mapboxgl-popup');
         if (popUps[0]) popUps[0].remove();
